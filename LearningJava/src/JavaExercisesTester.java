94ce134af7e79bab9d3fdb83f4e45006b4338593
EmrullahJack <emrullahjack@gmail.com>
2018-09-21 14:00:24 -0400

    New project for testing general java code

---
+++ LearningJava/src/JavaExercisesTester.java
@@ -0,0 +1,19 @@
+
+public class JavaExercisesTester {
+	
+	public static void main(String[] args) {
+		int[][] array = new int[3][4];
+		
+		for(int i = 0; i < 3; i++) {
+			
+			for(int j = 0; j < 4; j++) {
+				array[i][j] = 5;
+				System.out.println(array[i][j]);
+			}
+			
+		}
+		
+		
+	}
+	
+}
